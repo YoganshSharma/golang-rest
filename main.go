d3d9e0f5e084d0b9819d390558a1eebd9fac2abe
Yogansh Sharma <yoganshsharma65@gmail.com>
2024-03-09 17:37:30 +0530

    added form

--- main.go
+++ main.go
@@ -10,9 +10,16 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", IndexHandler)
+	r.HandleFunc("/form", FormHandler).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./index.html")
+}
+
+func FormHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO read data from the request
+	// TODO save the data
+	// TODO respond to the request Excercise for you
 }
